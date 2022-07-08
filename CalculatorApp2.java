9defd95d3664d04d057f99629ee2203da536a741
sonikaSM <108656130+sonikaSM@users.noreply.github.com>
2022-07-08 09:36:04 +0200

    Add files via upload

--- CalculatorApp2.java
+++ CalculatorApp2.java
@@ -5,21 +5,29 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
+
 public class CalculatorApp2 extends Application {
+    //label for displaying result
     @FXML
     private Label lb;
-
-
-
-    String a;
+    //checkers for testing
+    @FXML
+    private Label checker;
+    @FXML
+    private Label checker2;
+    //String a;
     double c,b;
+    // indicator for operations
     int indicator;
-    double res;
+    //indicator (for not duplicating ++//--**)
+    int indp=0;
+    int indm=0;
+    int indmt=0;
+    int indd=0;
 
     @Override
     public void start(Stage stage) throws IOException {
@@ -34,53 +42,55 @@ public class CalculatorApp2 extends Application {
     public static void main(String[] args) {
         launch();
     }
+    //checking if the first char of string is 0
+    private boolean check(){
+        if (lb.getText().equals("0")){
+            return true;
+        }else return false;
 
-
-    public void SetText(String str) {
-        String str2=lb.getText();
-        lb.setText(str2+str);
     }
-    public void OnClick1(MouseEvent event){
+    private void SetText(String str) {
+        if(check()){
+            //lb.setText("");
+            lb.setText(str);
+        }else {
+            String str2 = lb.getText();
+            lb.setText(str2 + str);
+        }
+    }
+    public void OnClick1(){
         SetText("1");
     }
 
-    public void OnClick2(MouseEvent event){
-
+    public void OnClick2(){
         SetText("2");
     }
-    public void OnClick3(MouseEvent event){
-
+    public void OnClick3(){
         SetText("3");
     }
-    public void OnClick4(MouseEvent event){
-
+    public void OnClick4(){
         SetText("4");
     }
-    public void OnClick5(MouseEvent event){
-
+    public void OnClick5(){
         SetText("5");
     }
-    public void OnClick6(MouseEvent event){
-
+    public void OnClick6(){
         SetText("6");
     }
-    public void OnClick7(MouseEvent event){
-
+    public void OnClick7(){
         SetText("7");
     }
-    public void OnClick8(MouseEvent event){
-
+    public void OnClick8(){
         SetText("8");
     }
-    public void OnClick9(MouseEvent event){
-
+    public void OnClick9(){
         SetText("9");
     }
-    public void OnClick0(MouseEvent event){
-
+    public void OnClick0(){
         SetText("0");
     }
 
+    //getting value of the variable (string is converting into double, and become clean)
     private double getValue(){
         double g;
         g=Double.parseDouble(lb.getText());
@@ -88,41 +98,108 @@ public class CalculatorApp2 extends Application {
         return g;
     }
 
-    public void OnClick_plus(MouseEvent event) {
-        c=getValue();
-        indicator=1;
-    }
-    public void OnClick_minus(MouseEvent event) {
-        c=getValue();
-        SetText("-");
-        indicator=1;
-    }
-    public void OnClick_div(MouseEvent event) {
-        c=getValue();
-        indicator=2;
-    }
-    public void OnClick_mult(MouseEvent event) {
-        c=getValue();
-        indicator=3;
-    }
-    public void OnClick_pt(MouseEvent event) {
-        SetText(".");
-    }
-    public void OnClick_eq(MouseEvent event) {
-        b=getValue();
-        double result=0;
+    public void OnClick_plus() {
+        indm = 0;
+        indmt = 0;
+        indd = 0;
 
-        switch (indicator){
-            case 1: {
-                result=plus(c,b);
-                lb.setText(String.valueOf(result));break;}
-            case (2): {result=div(c,b);lb.setText(String.valueOf(result));break;}
-            case (3): {result=mult(c,b);lb.setText(String.valueOf(result));break;}
-            default: lb.setText("Something wrong!");
+        if(indp==0) {
+            indp++;
+            c = getValue();
+            SetText("+");
+            indicator = 1;
+        }else {Call();}
+    }
+    public void OnClick_minus() {
+        indp = 0;
+        indmt = 0;
+        indd = 0;
+
+        if(indm==0) {
+            indm++;
+            c = getValue();
+            SetText("-");
+            indicator = 1;
+        }else {Call();}
+    }
+
+    private void Call() {
+        System.out.println("Stop!");
+    }
+
+    public void OnClick_div() {
+        indm=0;
+        indp=0;
+        indmt=0;
+
+        if(indd==0) {
+            indd++;
+            c = getValue();
+            indicator = 2;
+        }else {Call();}
+    }
+    public void OnClick_mult() {
+        indm=0;
+        indp=0;
+        indd=0;
+
+        if(indmt==0) {
+            indmt++;
+            c = getValue();
+            indicator = 3;
+        }else {Call();}
+    }
+    public void OnClick_pt() {
+        if(lb.getText().isEmpty()) SetText("0.") ;
+        else if ( !lb.getText().contains(".")) {
+            SetText(".");
+        }else{ System.out.println("Error. Invalid input");}
+    }
+    public void OnClick_eq() {
+        checker.setText(String.valueOf(c));
+
+        indm=0;
+        indp=0;
+        indmt=0;
+        indd = 0;
+
+        if(!lb.getText().isEmpty()) {
+
+            b = getValue();
+            checker2.setText(String.valueOf(b));
+
+            double result;
+
+            switch (indicator) {
+                case 1: {
+                    result = plus(c, b);
+                    lb.setText(String.valueOf(result));
+                    break;
+                }
+                case (2): {
+                    result = div(c, b);
+                    lb.setText(String.valueOf(result));
+                    break;
+                }
+                case (3): {
+                    result = mult(c, b);
+                    lb.setText(String.valueOf(result));
+                    break;
+                }
+                default:
+                    lb.setText("Something wrong!");
+            }
+
         }
-        res=result;
     }
+    public void OnClickBack(){
 
+        if(!lb.getText().isEmpty()){
+            StringBuilder str = new StringBuilder(lb.getText());
+            str.deleteCharAt(str.length()-1);
+            lb.setText(str.toString());
+        }else{ lb.setText("Error. Empty! ");}
+    }
     private double plus(double x,double y) {
         //System.out.println(x+y);
         return x+y;
@@ -136,7 +213,12 @@ public class CalculatorApp2 extends Application {
         //System.out.println(x*y);
         return x*y;
     }
-    public void onClickClear(MouseEvent event){
+    public void onClickClear(){
+        indm=0;
+        indp=0;
+        indmt=0;
+        indd = 0;
+
         lb.setText("");
     }
 
